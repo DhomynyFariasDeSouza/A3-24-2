b784521aa262902ca9ae2b23721be782c7104012
Dhomyny Ryan Farias De Souza <dhomyryan@outlook.com>
2024-11-21 14:57:41 -0300

    adicionando as classes para a autorização

---
+++ src/main/java/com/a3/api/Security/Role.java
@@ -0,0 +1,21 @@
+package com.a3.api.Security;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    private rolesName name;
+}
---
+++ src/main/java/com/a3/api/Security/rolesName.java
@@ -0,0 +1,8 @@
+package com.a3.api.Security;
+
+public enum rolesName {
+    ROLE_MESA,
+    ROLE_ADMINISTRACAO,
+    ROLE_ATENDENTE,
+    ROLE_COZINHA
+}
